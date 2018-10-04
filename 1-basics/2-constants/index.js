9d68b1931e02290e2e47728181302e4a15d79da2
Brendan Rosa <bren@Brendans-MacBook-Pro.local>
2018-10-04 13:12:45 -0400

    constants, primitives, dynamic types

---
+++ 1-basics/2-constants/index.js
@@ -0,0 +1,10 @@
+const interestRate = 0.5;
+interestRate = 1;
+console.log(interestRate);
+
+/**
+ * Notes
+ *
+ * Like variables, constants are final and are immutable.
+ * Re-assigning a value to a primitive constant throws an error on line 3.
+ */
