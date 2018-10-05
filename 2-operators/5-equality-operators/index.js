16cee769d1272d5610a9d72a8e1e8f66ce898afb
Brendan Rosa <bren@Brendans-MacBook-Pro.local>
2018-10-05 11:27:23 -0400

    Add DS_Store file to gitignore

---
+++ 2-operators/5-equality-operators/index.js
@@ -0,0 +1,7 @@
+// Strict Equality (Type + Value)
+console.log(1 === 1); // -> true
+console.log(1 === '1'); // -> false
+
+// Loose Equality (Value)
+console.log(1 == 1); // -> true
+console.log(1 == '1'); // -> true
