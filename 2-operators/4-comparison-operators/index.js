951cd7d3afbd7f6d534a5d2579867c6058972977
Brendan Rosa <bren@Brendans-MacBook-Pro.local>
2018-10-05 11:24:13 -0400

    basics > comparison operators

---
+++ 2-operators/4-comparison-operators/index.js
@@ -0,0 +1,11 @@
+let x = 1;
+
+// Relational operators
+console.log(x > 0);
+console.log(x >= 0);
+console.log(x < 1);
+console.log(x <= 1);
+
+// Equality operators
+console.log(x === 1); // Strict equality (type and value)
+console.log(x !== 1); // Strict inequality (type and value)
