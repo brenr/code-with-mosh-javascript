5d183360dd6d68aeb7b16ebfc507bcfb2628f9f7
Brendan Rosa <bren@Brendans-MacBook-Pro.local>
2018-10-05 11:18:57 -0400

    basics > operators, arithmetic operators

---
+++ 2-operators/1-javascript-operators/index.js
@@ -0,0 +1,9 @@
+/**
+ * Notes
+ *
+ * Operators essentially control the direction of the
+ * program's flow.
+ *
+ * There are arithmetic, assignment, comparison,
+ * logical, and bitwise operators.
+ */
