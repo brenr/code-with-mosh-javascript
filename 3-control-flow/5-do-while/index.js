5cd1591b7020073382a6ec8380588f70e953df77
Brendan Rosa <bren@Brendans-MacBook-Pro.local>
2018-10-11 12:47:35 -0400

    control flow > do while

---
+++ 3-control-flow/5-do-while/index.js
@@ -0,0 +1,14 @@
+/**
+ * Notes
+ *
+ * Do while loops are used to execute code at least once,
+ * regardless of the condition of the while.
+ */
+
+let i = 9;
+do {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+  i++;
+} while (i <= 5);
