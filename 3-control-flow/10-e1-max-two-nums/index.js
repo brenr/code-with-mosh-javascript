42b139b1c02a1cf2e40f8ad2e6e9720378a0a51f
Brendan Rosa <bren@Brendans-MacBook-Pro.local>
2018-10-11 13:09:32 -0400

    save

--- 3-control-flow/10-e1-max-two-nums/index.js
+++ 3-control-flow/10-e1-max-two-nums/index.js
@@ -2,6 +2,12 @@ let number = max(1, 2);
 console.log(number);
 
 function max(a, b) {
+  // Math class is better implementation
   // return Math.max(a,b);
   return a > b ? a : b;
 }
+
+/**
+ * When writing functions, have several test cases to ensure
+ * the full compatibility so no edge cases slip through.
+ */
