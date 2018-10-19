13c95749047b84394506bfc3a2729a19ca23666d
Brendan Rosa <bren@Brendans-MacBook-Pro.local>
2018-10-19 16:52:45 -0400

    objects > garbage collection

---
+++ 4-objects/10-garbage-collection/index.js
@@ -0,0 +1,10 @@
+let circle = {};
+console.log(circle);
+
+/**
+ * JavaScript engine has garbage collection
+ *
+ * Memory allocation is automatic, under the hood.
+ * Uses complex algorithms to determine what is no
+ * longer used and deallocates it from memory.
+ */
