0b66048fd64f7f30454d092f754b3171a4ed1b7d
brenr <brendan.rosa@gmail.com>
2018-11-12 19:33:04 -0500

    .

--- 4-objects/14-date/index.js
+++ 4-objects/14-date/index.js
@@ -1,3 +1,7 @@
 
 const now = new Date();
 const date1 = new Date('June 1 2018 10:00');
+const date2 = new Date(2018, 5, 1, 10); //Months are zero-based
+
+now.setFullYear(2017); // Modify the now object's year to 2017.
+now.toISOString(); // Common format used for timestamps in programming
